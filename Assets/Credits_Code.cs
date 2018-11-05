d2f15dec2ac929dd5ce7b528c3c75d539ab72fe1
jakobgarza01 <43855450+jakobgarza01@users.noreply.github.com>
2018-11-05 10:22:05 -0600

    Add files via upload

---
+++ Assets/Credits_Code.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Credits_Code : MonoBehaviour {
+
+    public void ChangeToScene (int sceneToChangeTo) { 
+        //SceneManagar.LoadScene[1];
+        Application.LoadLevel(sceneToChangeTo);
+    }
+    void Start () {
+		
+	}
+}
+
